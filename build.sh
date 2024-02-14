95fb31a84f589300bcde232b38fa4c0995a41b6f
Anand Mohan <andmhn@outlook.com>
2024-02-14 11:52:43 +0530

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+for arg in "$@"
+do
+    case $arg in
+      clean)
+        cd build/ && make clean && cd ..
+        cd build-emc && make clean
+        ;;
+      run)
+        ./build/chess
+        ;;
+      release)
+        cmake -B build -S . -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Release
+        cmake --build ./build && cp build/compile_commands.json .
+        ;;
+      debug)
+        cmake -B build -S . -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Debug
+        cmake --build ./build && cp build/compile_commands.json .
+
+        # generate ctags with dependency
+        echo "Generating tags..."
+        cc -M -I build/_deps/raylib-build/raylib/include/ `find sources -name "*.c"` |\
+            sed -e 's/[\\ ]/\n/g' | \
+            sed -e '/^$/d' -e '/\.o:[ \t]*$/d' | \
+            ctags -L - --c++-kinds=+p --fields=+ianS --extras=+q
+
+        ;;
+      web)
+        source "$HOME/repos/emsdk/emsdk_env.sh" || exit
+        mkdir -p build-emc 
+        # Ensure asset folder is copied
+        rm -rf build-emc/assets || true
+        cp -R assets build-emc/assets
+        export CPLUS_INCLUDE_PATH=
+        cd build-emc &&
+        emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html" &&
+        emmake make
+
+        # now update chess_web folder
+        cd ..
+        mkdir -p ../chess_web
+        rm -rf ../chess_web/*
+        cp build-emc/chess.* ../chess_web
+        mv ../chess_web/chess.html ../chess_web/index.html
+        cp -R assets ../chess_web
+        ;;
+      *)
+printf "Invalid arg \'$arg\'\n\
+Use these build options\n\
+-   clean\n\
+-   run\n\
+-   release\n\
+-   debug\n\
+-   web\n"
+        ;;
+    esac
+
+done
---
+++ sources/chess_main.c
@@ -0,0 +1,94 @@
+#include "raylib.h"
+
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
+//------------------------------------------------------------------------------
+// Global Variables Definition
+//------------------------------------------------------------------------------
+int screenWidth = 800;
+int screenHeight = 450;
+Texture2D texture;
+Sound fx_missile_launch;
+//------------------------------------------------------------------------------
+// Module Functions Declaration
+//------------------------------------------------------------------------------
+void UpdateDrawFrame(void); // Update and Draw one frame
+
+//------------------------------------------------------------------------------
+// Main Entry Point
+//------------------------------------------------------------------------------
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------
+#ifndef DEBUG
+    SetTraceLogLevel(LOG_WARNING);
+#endif
+
+    InitWindow(screenWidth, screenHeight, "Chess");
+    InitAudioDevice();
+
+    /// Load Assets
+    texture = LoadTexture(ASSETS_PATH "test.png");
+    fx_missile_launch = LoadSound(ASSETS_PATH "missile_launch.wav");
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    SetExitKey(0);
+    //--------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    // De-Initialization
+    //--------------------------------------------------------------------------
+    UnloadSound(fx_missile_launch);
+    CloseAudioDevice();
+    CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------
+
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+// Module Functions Definition
+//------------------------------------------------------------------------------
+void UpdateDrawFrame(void)
+{
+    // Update
+    //--------------------------------------------------------------------------
+    if (IsKeyPressed(KEY_ENTER))
+        PlaySound(fx_missile_launch); // Play missile
+
+    // Draw
+    //--------------------------------------------------------------------------
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    const int texture_x = screenWidth / 2 - texture.width / 2;
+    const int texture_y = screenHeight / 2 - texture.height / 2;
+    DrawTexture(texture, texture_x, texture_y, WHITE);
+
+    const char *text = "PRESS ENTER!";
+    const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
+    DrawText(text, (int)(screenWidth / 2) - text_size.x / 2,
+             texture_y + texture.height + text_size.y + 10, 20, BLACK);
+
+    // Draw human
+    DrawCircle(100, 100, 50, YELLOW);
+    DrawCircle(100, 100, 45, GREEN);
+    DrawCircle(90, 100, 5, BLACK);
+    DrawCircle(110, 100, 5, BLACK);
+    DrawRectangle(90, 115, 20, 10, RED);
+    DrawRectangle(50, 150, 100, 100, SKYBLUE);
+    EndDrawing();
+    //--------------------------------------------------------------------------
+}
